16c840da20f27f6b7419bd591d693cbb794a501d
Denisse Oliva <yuleiditho@gmail.com>
2025-12-16 01:19:02 -0600

    fix(user): reorder fillable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,12 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'google_id',
-        'avatar',
         'name',
         'email',
         'password',
-          
+        'google_id',
+        'avatar',
     ];
 
     /**
